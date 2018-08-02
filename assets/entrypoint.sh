28d0a86e1de7a1b1b275546ec7c246799f2c8005
Sascha Spreitzer <sascha@redhat.com>
2018-08-02 16:14:48 +0200

    Add runtime announce config

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -9,4 +9,8 @@ if [ ! -d "/var/lib/ipfs/.ipfs" ]; then
   ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
 fi
 
+if [ -n "${IPFS_ANNOUNCE}" ]; then
+  ipfs.config --json Addresses.Announce "${IPFS_ANNOUNCE}"
+fi
+
 exec $*
