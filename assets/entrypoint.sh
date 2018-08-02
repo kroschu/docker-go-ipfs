853e667569d4bb6b282db5ca1d435ddf86fe7a90
Sascha Spreitzer <sascha@redhat.com>
2018-08-02 13:02:32 +0200

    Fix typo

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 if [ ! -d "/var/lib/ipfs/.ipfs" ]; then
   ipfs init
   ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
-  ipfs config Addesses.Gateway /ip4/0.0.0.0/tcp/8080
+  ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
 fi
 
 exec $*
