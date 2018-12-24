58e42ebf68150b1928cacc5d4ceb68f211b41563
kroschu <tukroschu@gmail.com>
2018-12-24 23:08:01 +0200

    Update entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -7,6 +7,10 @@ if [ ! -d "/var/lib/ipfs/.ipfs" ]; then
   ipfs init
   ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
   ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+  ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ # ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://192.168.1.228", "http://fcloud.pp.ua:5001", "http://localhost", "http://127.0.0.1"]'
+  ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+  ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
 fi
 
 if [ -n "${IPFS_ANNOUNCE}" ]; then
