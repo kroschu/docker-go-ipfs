ab08455dc0eb4edd8225ad51b0b69aa0d634a279
kroschu <tukroschu@gmail.com>
2018-12-26 11:53:22 +0200

    Update entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 if [ ! -d "/var/lib/ipfs/.ipfs" ]; then
-  ipfs init
+  ipfs init --profile=server
   ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
   ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
   ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
